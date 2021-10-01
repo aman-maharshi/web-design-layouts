c4e283894a6183636bd901a6213de635e14c00f9
aman-maharshi <amanm4harshi@gmail.com>
2021-10-01 15:25:01 +0530

    sroll to top btn with throttled scroll event listner

--- responsive-cv/script.js
+++ responsive-cv/script.js
@@ -1,23 +1,59 @@
-let html = document.querySelector("html")
-let toggleBtn = document.querySelector(".theme-toggle-btn")
+/*
+    Theme Toggle
+-------------------*/
+const html = document.querySelector("html")
+const themeToggleBtn = document.querySelector(".theme-toggle-btn")
 
 const defaultTheme = localStorage.getItem("theme")
 
 if (defaultTheme === "dark") {
     html.setAttribute("theme", "dark")
-    toggleBtn.innerHTML = '<box-icon name="sun" color="var(--title-color)"></box-icon>'
+    themeToggleBtn.innerHTML = '<box-icon name="sun" color="var(--title-color)"></box-icon>'
 }
 
-toggleBtn.addEventListener("click", changeTheme)
+themeToggleBtn.addEventListener("click", changeTheme)
 
 function changeTheme() {
     if (html.getAttribute("theme") == "light") {
-        toggleBtn.innerHTML = '<box-icon name="sun" color="var(--title-color)"></box-icon>'
+        themeToggleBtn.innerHTML = '<box-icon name="sun" color="var(--title-color)"></box-icon>'
         html.setAttribute("theme", "dark")
         localStorage.setItem("theme", "dark")
     } else {
-        toggleBtn.innerHTML = '<box-icon name="moon" color="var(--title-color)"></box-icon>'
+        themeToggleBtn.innerHTML = '<box-icon name="moon" color="var(--title-color)"></box-icon>'
         html.setAttribute("theme", "light")
         localStorage.setItem("theme", "light")
     }
 }
+
+/*
+    Scroll to Top
+-------------------*/
+const scrollToTopBtn = document.querySelector(".scroll-to-top")
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
+
+toggleScrollToTopButton = () => {
+    if (window.innerWidth <= 768) {
+        // console.log(document.documentElement.scrollTop)
+        if (document.documentElement.scrollTop <= 300) {
+            scrollToTopBtn.style.display = "none"
+        } else {
+            scrollToTopBtn.style.display = "block"
+        }
+    }
+}
+
+let timer
+const debounceScroll = () => {
+    clearTimeout(timer)
+    timer = setTimeout(toggleScrollToTopButton, 100)
+}
+
+// document.addEventListener("scroll", toggleScrollToTopButton)
+document.addEventListener("scroll", debounceScroll)
