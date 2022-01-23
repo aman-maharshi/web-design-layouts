da738200e3ace05059810586fd21be92bc5a197a
aman-maharshi <amanm4harshi@gmail.com>
2022-01-23 23:24:10 +0530

    homepage complete

--- tailwind-portfolio/tailwind.config.js
+++ tailwind-portfolio/tailwind.config.js
@@ -3,13 +3,11 @@ module.exports = {
     theme: {
         colors: {
             body: "#17171F",
-            "selected-text": "#A3A3FF",
-            theme: "#3F3FFF",
+            "selected-text": "#2dd4bf",
+            theme: "#14b8a6",
             nav: "#404053",
             secondary: "#9191A4",
             badge: "#3F3F51",
-            "input-border": "#565666",
-            input: "#2A2A35",
             white: "#fff"
         },
         fontFamily: {
