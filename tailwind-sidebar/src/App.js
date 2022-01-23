f8ed2d9db58a3aa6ae5bd302fbcc5bc1f8ae438e
aman-maharshi <amanm4harshi@gmail.com>
2022-01-24 00:04:39 +0530

    init

---
+++ tailwind-sidebar/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, enim.</p>
+            <button className="border p-5 mt-10">Lorem</button>
+        </div>
+    )
+}
+
+export default App
